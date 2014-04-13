60e40b44d62a0f5d7e3357a7ba266be0ec366e4f
iValou <valentin.lhommeau@gmail.com>
2014-04-13 14:51:17 +0200

    Initial commit

---
+++ Symfony/src/tuto/WelcomeBundle/Controller/HomepageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tuto\WelcomeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomepageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('tutoWelcomeBundle:Default:index.html.twig');
+    }
+}
